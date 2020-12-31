ec159094e761ac08cbf02ae2ae38f1ede7736af4
Arunav <arunav@vonome.com>
2020-12-31 19:21:33 +0600

    Create MenuItemController and Index action with View

---
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            _hostEnvironment = hostEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItems
+                .Include(m=>m.Category)
+                .Include(m=>m.SubCategory)
+                .ToListAsync();
+
+            return View(menuItems);
+        }
+    }
+}
