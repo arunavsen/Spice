da329eb57d4bd9caf66c3d84922451ca7b404bfc
Arunav <arunav@vonome.com>
2021-01-14 12:20:25 +0600

    Menu Item Create View (POST)+(Image) How to upload an image and rename it on the basis of Id name

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models;
 using Spice.Models.ViewModel;
+using Spice.Utility;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -45,6 +47,51 @@ namespace Spice.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View(MenuItemVM);
+        }
+
+        [HttpPost,ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST()
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItems.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+            // Work on the image saving section
+            string webRootPath = _hostEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuITemFromDb = await _db.MenuItems.FindAsync(MenuItemVM.MenuItem.Id);
+
+            if (files.Count>0)
+            {
+                // File has been uploaded
+                var uploads = Path.Combine(webRootPath,"images");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filesStream = new FileStream(Path.Combine(uploads,MenuItemVM.MenuItem.Id+extension),FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+
+                menuITemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                // No file was uploaded. So use default.
+                var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads,webRootPath+@"\images\"+MenuItemVM.MenuItem.Id+".png");
+                menuITemFromDb.Image = @"\images\" + menuITemFromDb.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
