13471a68be4139c458288e7ad7f3670833a43ada
Arunav <arunav@vonome.com>
2021-02-07 00:58:36 +0600

    Changes to Register Page

--- Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -63,6 +63,7 @@ namespace Spice.Areas.Identity.Pages.Account
 
             [Required]
             public string Name { get; set; }
+            public string PhoneNumber { get; set; }
             public string StreetAddress { get; set; }
             public string City { get; set; }
             public string State { get; set; }
